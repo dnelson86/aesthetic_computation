22b98605a1cd502a1eeedfa960ade51d3f710dfb
Dylan Nelson <dnelson@mpa-garching.mpg.de>
2019-05-04 18:20:59 +0200

    Fix hover issue for touch (mobile/ipad) devices, and add touch swipe support for navigation similarly to keyboard left/right.

--- static/ac/js/site.js
+++ static/ac/js/site.js
@@ -49,4 +49,64 @@
 
     });
 
+    // detect touch swipes
+    $(this).bind('touchstart', touchstart);
+      
+    function touchstart(event) {
+      var touches = event.originalEvent.touches;
+      if (touches && touches.length) {
+        startX = touches[0].pageX;
+        startY = touches[0].pageY;
+        $(this).bind('touchmove', touchmove);
+        $(this).bind('touchend', touchend);
+      }
+      event.preventDefault();
+    }
+      
+    function touchmove(event) {
+      var touches = event.originalEvent.touches;
+      if (touches && touches.length) {
+        var deltaX = startX - touches[0].pageX;
+        var deltaY = startY - touches[0].pageY;
+          
+        if (deltaX >= 50) {
+          //$this.trigger("swipeLeft");
+          // individual post page: arrow keys trigger page-redirect navigation
+          var prev_link = $("#prev_link");
+          if (prev_link) {
+            window.location.replace(prev_link.attr("href"));
+          }
+        }
+
+        if (deltaX <= -50) {
+          // individual post page: arrow keys trigger page-redirect navigation
+          var next_link = $("#next_link");
+          if (next_link) {
+            window.location.replace(next_link.attr("href"));
+          }
+          //$this.trigger("swipeRight");
+        }
+
+        //if (deltaY >= 50) {
+        //  $this.trigger("swipeUp");
+        //}
+
+        //if (deltaY <= -50) {
+        //  $this.trigger("swipeDown");
+        //}
+
+        if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+          $(this).unbind('touchmove', touchmove);
+          $(this).unbind('touchend', touchend);
+        }
+      }
+      event.preventDefault();
+    }
+      
+    function touchend(event) {
+      $(this).unbind('touchmove', touchmove);
+      event.preventDefault();
+    }
+      
+
 })(jQuery);
